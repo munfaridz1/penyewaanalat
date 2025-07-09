c262abdb05f107ba321a5b5d4cc8f9b061f2ea86
Munfaridz1 <220101016@mhs.udb.ac.id>
2025-07-09 12:41:59 -0700

    first commit

--- api/index.php
+++ api/index.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Beranda Penyewaan Alat</title>
-  <link rel="stylesheet" href="/assets/style.css">
-</head>
-<body>
-  <h1>INFORMASI KATEGORI LAPTOP</h1>
-  <nav>
-    <a href="/">Beranda</a>
-    <a href="/kategori">Kategori Laptop</a>
-    <a href="/merk">Merk Laptop</a>
-  </nav>
-  <p>Selamat Datang di Aplikasi Penyewaan Outdoor!</p>
-</body>
-</html>
+<?php
+
+require __DIR__ . '/../public/index.php';
